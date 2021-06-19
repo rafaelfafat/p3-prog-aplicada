92853b576ea6f8d475c1d1a789429f94cecfec82
RafaelSantos5 <52930792+rafaelfafat@users.noreply.github.com>
2021-06-19 22:39:28 +0000

    Initial commit

---
+++ complexo.h
@@ -0,0 +1,20 @@
+#ifndef COMPLEXO_H
+#define COMPLEXO_H
+#endif
+
+using namespace std;
+
+typedef struct{
+  void print(int img1, int img2, int real1, int real2){
+    std:: cout << "-----ENTRADAS-----" << endl;
+    if (img1 > 0){cout << " n1 = " << real1 << "+" << img1 << "i" << endl;}
+    else{cout << " n1 = " << real1 << img1 << "i" << endl;}
+    if (img2 > 0){cout << " n2 = " << real2 << "+" << img2 << "i" << endl;}
+    else{cout << " n2 = " << real2 << img2 << "i" << endl;}
+  }
+  int Soma(int x, int y){
+    int soma = x + y;
+    return soma;
+  }
+
+}tComplexos;
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "complexo.h"
+
+using namespace std;
+
+int main() {
+  tComplexos nComplexo;
+  int real1, img1, real2, img2;
+  
+  cout << "Digite a parte real do primeiro numero: "; cin >> real1;
+  cout << "Digite a parte imaginaria do primeiro numero: "; cin >> img1;
+  cout << "Digite a parte real do segundo numero: "; cin >> real2;
+  cout << "Digite a parte imaginaria do segundo numero: "; cin >> img2;
+
+  nComplexo.print(img1, img2, real1, real2);
+
+}
